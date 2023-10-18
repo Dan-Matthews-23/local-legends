59beffd45c9c27ad2aa915fb69b674b7aa1d2f32
Dan-Matthews-23 <>
2023-10-18 07:52:55 +0000

    CREATED register.html with placeholder content, UPDATED style.css with placeholder formatting for register

--- local_legends/routes.py
+++ local_legends/routes.py
@@ -5,3 +5,7 @@ from local_legends import app, db
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/register.html")
+def register():
+    return render_template("register.html")
