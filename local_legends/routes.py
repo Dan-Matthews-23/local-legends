ba165c307cd5e15e6c8bffdc60cc15dfefcaf77f
Dan-Matthews-23 <>
2023-10-18 14:28:31 +0000

    UPDATED readme with new STage Three content

--- local_legends/routes.py
+++ local_legends/routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 from local_legends import app, db
 @app.route("/")
 def home():
-    return render_template("register.html")
+    return render_template("restaurants.html")
 
 ## @app.route("/register")
 ## def register():
