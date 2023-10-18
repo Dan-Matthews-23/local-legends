8e91de2e233a9a50cf8f6b3a22455bb252353ed1
Dan-Matthews-23 <>
2023-10-18 14:16:56 +0000

    UPDATED register.html, style.css with placeholder text for Stage Three

--- local_legends/routes.py
+++ local_legends/routes.py
@@ -1,11 +1,9 @@
 from flask import render_template
 from local_legends import app, db
-
-
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/register.html")
-def register():
     return render_template("register.html")
+
+## @app.route("/register")
+## def register():
+##    return render_template("register.html")
