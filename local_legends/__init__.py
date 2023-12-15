4d53ee15471e35e11d89db55168c931a76b3e4af
Dan-Matthews-23 <>
2023-12-15 17:36:46 +0000

    UPDATED __init__ to correct errors with indentation

--- local_legends/__init__.py
+++ local_legends/__init__.py
@@ -13,10 +13,10 @@ app = Flask(__name__)
  if os.environ.get("DEVELOPMENT") == "True":
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
  else:
-     uri = os.environ.get("DATABASE_URL")
-     if uri.startswith("postgres://"):
-         uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
