807cd9d6fd5328a5890ea7dc8ecc2f7f3561f895
Dan-Matthews-23 <>
2023-12-15 17:59:22 +0000

    UPDATED __init__ to correct errors with indentation

--- local_legends/__init__.py
+++ local_legends/__init__.py
@@ -7,17 +7,21 @@ if os.path.exists("env.py"):
     import env  # noqa
 
 
+if os.path.exists("env.py"):
+    import env  # noqa
 app = Flask(__name__)
+# Database configuration
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+# Additional database configuration options
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# Initialize Flask extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
