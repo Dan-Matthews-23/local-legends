8beefa4217f36b58237a527a28af91394d323d93
Dan-Matthews-23 <>
2024-01-07 17:18:42 +0000

    UPDATED routes with correction to delete_review

--- local_legends/routes.py
+++ local_legends/routes.py
@@ -1211,46 +1211,46 @@ def delete_review(review_id):
             else:
                 average_taste_stars = []
                 for review in existing_reviews:
-                    average_taste_stars.append(float(Reviews.taste_stars))
+                    average_taste_stars.append(Reviews.taste_stars)
                 before_round_average_taste_stars = mean(average_taste_stars)
                 average_taste_stars = round(
                     before_round_average_taste_stars, 1)
                 average_presentation_stars = []
                 for review in existing_reviews:
-                    average_presentation_stars.append(float(
-                        Reviews.presentation_stars))
+                    average_presentation_stars.append(
+                        Reviews.presentation_stars)
                 before_round_average_presentation_stars = mean(
                     average_presentation_stars)
                 average_presentation_stars = round(
                     before_round_average_presentation_stars, 1)
                 average_friendliness_stars = []
                 for review in existing_reviews:
-                    average_friendliness_stars.append(float(
-                        Reviews.friendliness_stars))
+                    average_friendliness_stars.append(
+                        Reviews.friendliness_stars)
                 before_round_average_friendliness_stars = mean(
                     average_friendliness_stars)
                 average_friendliness_stars = round(
                     before_round_average_friendliness_stars, 1)
                 average_ambience_stars = []
                 for review in existing_reviews:
-                    average_ambience_stars.append(float(
-                        Reviews.ambience_stars))
+                    average_ambience_stars.append(
+                        Reviews.ambience_stars)
                 before_round_average_ambience_stars = mean(
                     average_ambience_stars)
                 average_ambience_stars = round(
                     before_round_average_ambience_stars, 1)
                 average_price_stars = []
                 for review in existing_reviews:
-                    average_price_stars.append(float(
-                        Reviews.price_stars))
+                    average_price_stars.append(
+                        Reviews.price_stars)
                 before_round_average_price_stars = mean(
                     average_price_stars)
                 average_price_stars = round(
                     before_round_average_price_stars, 1)
                 average_overall_stars = []
                 for review in existing_reviews:
-                    average_overall_stars.append(float(
-                        Reviews.overall_stars))
+                    average_overall_stars.append(
+                        Reviews.overall_stars)
                 before_round_average_overall_stars = mean(
                     average_overall_stars)
                 average_overall_stars = round(
